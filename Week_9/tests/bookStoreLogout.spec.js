be70137b021f747e3d0419235e971b86aa17d3d1
lkluchnik <lkluchnik@amcbridge.com>
2026-09-08 14:34:14 +0300

    Cleanup

--- Week_9/tests/bookStoreLogout.spec.js
+++
@@ -1,6 +0,0 @@
-const { test } = require('../fixtures/app.fixture');
-
-test('BookStoreLogout', async ({ loggedInBookStore }) => {
-  await loggedInBookStore.clickLogout();
-  await loggedInBookStore.expectLoginInBookStoreVisible();
-});
